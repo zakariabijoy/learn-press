39356fd055010548194aac9be27d9ad03b1e3711
zakariabijoy <zakaria.bijoy@live.com>
2019-11-23 01:53:55 +0600

    queue is add to mail sending system(but queue connection type is  still sync because of redis installation problem is windows)

--- app/Notifications/NewCourseAdded.php
+++ app/Notifications/NewCourseAdded.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class NewCourseAdded extends Notification
+class NewCourseAdded extends Notification implements ShouldQueue
 {
     use Queueable;
 
